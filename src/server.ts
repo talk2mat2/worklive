fedb3bc62435555f87c9bd61d675685af96c90ac
martins chukwuma <martins.chukwuma@stanbicibtc.com>
2026-04-26 07:00:19 +0100

    fixes

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
     if (process.env.NODE_ENV !== "production") {
       await sequelize.sync(); // only in dev/test
     }
-    // await sequelize.sync();
+    await sequelize.sync();
 
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
